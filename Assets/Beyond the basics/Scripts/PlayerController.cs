050f208b168e36d0342e6f88f38cf8fe6f951f29
Hernando NJ <hernandonj.unity@gmail.com>
2021-02-06 21:21:46 -0500

    Revert "Revert "PlayerPrefab updated because previous was some issues""
    This reverts commit cfe370e6bce5bc8968eb1008854a311440cf4b9b.

---
+++ Assets/Beyond the basics/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerController : Shape
+{
+    public ProjectileController projectilePrefab;
+
+    protected override void Start()
+    {
+        base.Start();
+        SetColor(Color.cyan);
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            FireProjectile();
+    }
+
+    private void MovePlayer()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if(Mathf.Abs(moveHorizontal) > Mathf.Epsilon)
+        {
+            moveHorizontal = moveHorizontal * Time.deltaTime  * gameSceneController.playerSpeed;
+            moveHorizontal += transform.position.x;
+
+            float maxRightBound = gameSceneController.screenBounds.x - halfWidth;
+            float minLeftBound = -maxRightBound;
+            float limit = Mathf.Clamp(moveHorizontal, minLeftBound, maxRightBound);
+
+            transform.position = new Vector2(limit, transform.position.y);
+        }
+    }
+
+    private void FireProjectile()
+    {
+        Vector2 spawnPosition = transform.position;
+
+        ProjectileController projectile = Instantiate(projectilePrefab, spawnPosition, Quaternion.identity);
+
+        projectile.projectileSpeed = 2;
+        projectile.projectileDirection = Vector2.up;
+    }
+}
